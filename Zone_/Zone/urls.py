f7e36d112dcbfae0c6d19345681dfde2c6aa2058
Mr-mnin <nadimul.nahin008@gmail.com>
2026-06-15 11:24:42 +0600

    fixed bug

--- Zone_/Zone/urls.py
+++ Zone_/Zone/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('play/', include('play.urls')),
-    path('newyear', include('newyear.urls'))
+    path('newyear/', include('newyear.urls'))
 ]
