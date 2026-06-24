3cbea2b0895cea3391a97862c1543d7b6bb5bf5c
Mr-mnin <nadimul.nahin008@gmail.com>
2026-06-24 16:36:53 +0600

    merged in home

--- Counter/count/static/count/counter.js
+++ Counter/count/static/count/counter.js
@@ -3,14 +3,12 @@ let count = 0;
 function counter() {
     count++;
     heading = document.querySelector("h1");
-    heading.innerHTML = count
+    heading.innerHTML = count;
     if (count % 10 === 0) {
-        alert('Count is now ${ count }');
+        alert(`Count is now ${count}`);
     }
-
-    document.addEventListener("DOMContentLoaded", function () {
-        document.querySelector('button').onclick = counter;
-    }); 
-
-    
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('button').onclick = counter;
+});
