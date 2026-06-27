3dd47cfe11a64bb702e8b697a5b7003559326d04
Mr-mnin <nadimul.nahin008@gmail.com>
2026-06-27 12:44:06 +0600

    dom manupulation com1

--- Counter/count/static/count/hello.js
+++ Counter/count/static/count/hello.js
@@ -1,16 +1,25 @@
-function hello() {
-  heading = document.querySelector('h1');
-  if (heading.innerHTML === "Hello World") {
-    heading.innerHTML = "Goodbye!";
-  } else {
-    heading.innerHTML = "Hello World";
-  }
- 
-}
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("form") = function () {
+  document.querySelector("button").onclick = function () {
+    heading = document.querySelector("h1");
+    if (heading.innerHTML === "Hello World") {
+      heading.innerHTML = "Goodbye!";
+    } else {
+      heading.innerHTML = "Hello World";
+    }
+  };
+
+  document.querySelector("form").onsubmit =  () => {
     const name = document.querySelector("#name").value;
-    alert(`Hello ${name}!`)
-    
-  }
+    alert(`Hello ${name}`);
+  };
+
+});
+
+document.addEventListener("DOMContentLoaded",  () => {
+  //change fontcolor 
+  document.querySelectorAll('button').forEach(button => {
+    button.onclick = () =>{
+      document.querySelector('#hello').style.color = button.dataset.color
+    }
+  })
 })
