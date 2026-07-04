5ec0e89e17967f42556aae7a85ecf36a213e8666
Mr-mnin <nadimul.nahin008@gmail.com>
2026-07-04 12:10:17 +0600

    fixComptibility

--- Task/currency/urls.py
+++ Task/currency/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('currency0/', views.currency0, name='currency0'),
-    path('currency1/', views.currency1, name='currency1'),
-    path('api/rates/', views.currency_proxy, name='currency_proxy'),
+    path("currency0/", views.currency0, name="currency0"),
+    path("currency1/", views.currency1, name="currency1"),
+    path("api/rates/", views.currency_proxy, name="currency_proxy"),
 ]
